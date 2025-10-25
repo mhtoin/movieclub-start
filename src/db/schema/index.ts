bab342de7d7212a0b4f5dd60af8ef9d13126999c
Miika Oinonen <mhtoin@utu.fi>
2025-10-26 01:56:33 +0300

    Setup local postgres for development

---
+++ src/db/schema/index.ts
@@ -0,0 +1,11 @@
+// Export all tables
+export * from './movies';
+export * from './raffles';
+export * from './sessions';
+export * from './shortlists';
+export * from './siteconfig';
+export * from './tierlists';
+export * from './users';
+
+// Export relations
+export * from './relations';
