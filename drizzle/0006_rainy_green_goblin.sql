3d2d6458a94b727b7d5233e45a36cca0d3f5e954
Miika Oinonen <mhtoin@utu.fi>
2026-03-02 16:01:45 +0200

    Fix migration

--- drizzle/0006_rainy_green_goblin.sql
+++ drizzle/0006_rainy_green_goblin.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "password_reset_token" (
+CREATE TABLE IF NOT EXISTS "password_reset_token" (
 	"id" text PRIMARY KEY NOT NULL,
 	"userId" text NOT NULL,
 	"token" text NOT NULL,
@@ -6,10 +6,10 @@ CREATE TABLE "password_reset_token" (
 	CONSTRAINT "password_reset_token_token_unique" UNIQUE("token")
 );
 --> statement-breakpoint
-ALTER TABLE "tierlist" ADD COLUMN "watchDateFrom" text;--> statement-breakpoint
-ALTER TABLE "tierlist" ADD COLUMN "watchDateTo" text;--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "colorScheme" text DEFAULT 'default' NOT NULL;--> statement-breakpoint
+ALTER TABLE "tierlist" ADD COLUMN IF NOT EXISTS "watchDateFrom" text;--> statement-breakpoint
+ALTER TABLE "tierlist" ADD COLUMN IF NOT EXISTS "watchDateTo" text;--> statement-breakpoint
+ALTER TABLE "user" ADD COLUMN IF NOT EXISTS "colorScheme" text DEFAULT 'default' NOT NULL;--> statement-breakpoint
 ALTER TABLE "password_reset_token" ADD CONSTRAINT "password_reset_token_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "movie_watchDate_idx" ON "movie" USING btree ("watchDate");--> statement-breakpoint
-CREATE INDEX "movie_userId_idx" ON "movie" USING btree ("userId");--> statement-breakpoint
-ALTER TABLE "tierlist" DROP COLUMN "watchDate";
+CREATE INDEX IF NOT EXISTS "movie_watchDate_idx" ON "movie" USING btree ("watchDate");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "movie_userId_idx" ON "movie" USING btree ("userId");--> statement-breakpoint
+ALTER TABLE "tierlist" DROP COLUMN IF EXISTS "watchDate";
