c9f64a380a3b64a6b96aaa6945854b4785399153
Miika Oinonen <mhtoin@utu.fi>
2026-03-10 17:06:47 +0200

    Singleton site config

--- src/db/schema/siteconfig.ts
+++ src/db/schema/siteconfig.ts
@@ -1,7 +1,12 @@
-import { jsonb, pgTable, text } from "drizzle-orm/pg-core";
+import { sql } from 'drizzle-orm'
+import { jsonb, pgTable, text, uniqueIndex } from 'drizzle-orm/pg-core'
 
-export const siteConfig = pgTable("site_config", {
+export const siteConfig = pgTable(
+  'site_config',
+  {
     id: text().primaryKey().notNull(),
     watchProviders: jsonb(),
     watchWeekDay: text().notNull(),
-});
+  },
+  () => [uniqueIndex('site_config_singleton').using('btree', sql`(TRUE)`)],
+)
