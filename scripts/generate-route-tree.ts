ea2777b273aad9e8a5acafe13855e19768e1d252
Miika Oinonen <mhtoin@utu.fi>
2026-05-14 23:15:10 +0300

    Fix ci, explicitly generate route tree

---
+++ scripts/generate-route-tree.ts
@@ -0,0 +1,6 @@
+import { Generator, getConfig } from '@tanstack/router-generator'
+
+const config = getConfig({}, process.cwd())
+const generator = new Generator({ config, root: process.cwd() })
+await generator.run()
+console.log('Generated route tree')
